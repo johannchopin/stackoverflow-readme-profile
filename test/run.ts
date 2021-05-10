b95b34fefd19ed928d35e1646af61a77eaeb48b5
johannchopin <johannchopin@pm.me>
2021-05-10 23:23:36 +0200

    🚧 Use my id

--- test/run.ts
+++ test/run.ts
@@ -3,7 +3,7 @@ import * as path from 'path'
 
 import { getProfileSvg } from '../src'
 
-getProfileSvg(858369).then((svg) => {
+getProfileSvg(8583669).then((svg) => {
   fs.writeFileSync(path.resolve(__dirname, './profile.svg'), svg)
 }).catch((error) => {
   console.log(error)

dfda31002435e91d7cc91039a40eb2fc52df6757
johannchopin <johannchopin@pm.me>
2021-05-10 17:19:54 +0200

    🚧

--- test/run.ts
+++ test/run.ts
@@ -3,7 +3,7 @@ import * as path from 'path'
 
 import { getProfileSvg } from '../src'
 
-getProfileSvg(8583669).then((svg) => {
+getProfileSvg(858369).then((svg) => {
   fs.writeFileSync(path.resolve(__dirname, './profile.svg'), svg)
 }).catch((error) => {
   console.log(error)

05d0fa20a81a4fac9e63cc6067100b0bb2db3de4
johannchopin <johannchopin@pm.me>
2021-06-01 16:06:46 +0200

    ✅ templates

---
+++ src/__tests__/templates.js
@@ -0,0 +1,25 @@
+import { renderError, renderProfile } from '../templates'
+
+describe('Templates functions', () => {
+  describe('error svg template', () => {
+    it('should match the snapshot', () => {
+      expect(renderError({error: 'Error: Unable to fetch the user with the id 858453669'})).toMatchSnapshot()
+    })
+  })
+  describe('profile svg template', () => {
+    it('should match the snapshot', () => {
+      expect(renderProfile({
+        avatarLink: 'foobar',
+        username: 'foobar',
+        reputation: '42k',
+        badges: {
+          gold: 42,
+          silver: 42,
+          bronze: 42
+        },
+        location: 'foobar',
+        website: 'foobar'
+      })).toMatchSnapshot()
+    })
+  })
+})

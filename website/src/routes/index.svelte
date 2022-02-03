a2b17c8b1deebe89500c5e18e49bad86df1f82d1
johannchopin <johannchopin@pm.me>
2022-02-03 22:36:00 +0100

    🎉 Init website

---
+++ website/src/routes/index.svelte
@@ -0,0 +1,20 @@
+<svelte:body class=""/>
+
+<main class="container">
+  <h1 class="mb-0 mt-5">stackoverflow-readme-profile</h1>
+  <p class="ms-3">
+    <span class="fw-bold text-primary me-1">&#x3e;</span>
+    Easily share your Stackoverflow's profile on your README
+  </p>
+
+  <div class="form-floating mb-3">
+    <input type="number" class="form-control bg-dark text-light" id="soId" placeholder="8583669">
+    <label for="soId">Your SO identifier (ex: 8583669)</label>
+  </div>
+</main>
+
+<style lang="scss">
+  h1 {
+    font-size: 2.5em;
+  }
+</style>

95c8594d225d39571b6256b4822584ebf41205b9
johannchopin <johannchopin@pm.me>
2022-02-07 23:47:51 +0100

    💄 Show Copy button on hover or focus

--- website/src/lib/components/Code.svelte
+++ website/src/lib/components/Code.svelte
@@ -10,3 +10,9 @@
   <pre class="rounded bg-dark p-3"><code>{code}</code></pre>
   <CopyButton class="position-absolute top-0 end-0 m-2" onSave={() => { setClipboard(code) }} />
 </div>
+
+<style>
+  div:not(:hover) :global(button:not(:focus):not(:focus-within)) {
+    opacity: 0;
+  }
+</style>

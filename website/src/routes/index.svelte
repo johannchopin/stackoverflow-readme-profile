7264fab60384fcf763d005f56ca01181e506aa9e
johannchopin <johannchopin@pm.me>
2022-02-12 10:51:18 +0100

    💄 Improve styles

--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -15,7 +15,7 @@
 <main class="container pt-0 pt-md-5">
   <SocialLinks class="mt-5" />
 
-  <h1 class="mb-0 mt-3">stackoverflow-readme-profile</h1>
+  <h1 class="mb-0 mt-3 fs-3 fw-bold">stackoverflow-readme-profile</h1>
   <p class="ms-3">
     <span class="fw-bold text-primary me-1">&#x3e;</span>
     Easily share your Stackoverflow's profile on your README
@@ -42,7 +42,13 @@
 <Footer />
 
 <style lang="scss">
-  h1 {
-    font-size: 2.5em;
+  @import '../../node_modules/bootstrap/scss/functions';
+  @import '../../node_modules/bootstrap/scss/variables';
+  @import '../../node_modules/bootstrap/scss/mixins';
+
+  @include media-breakpoint-up(md) {
+    h1 {
+      font-size: 2.5em!important;
+    }
   }
 </style>

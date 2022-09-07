520f7b6d82039c693efd27f45696abd246135dc8
johannchopin <johannchopin@pm.me>
2022-09-07 22:06:24 +0200

    fetch tag detail

--- website/src/routes/badges-league/index.svelte
+++ website/src/routes/badges-league/index.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { fade } from "svelte/animate";
+
+  import { page } from "$app/stores";
 
   import { API_BASEURL } from "$lib/constants";
 
   let tags: string[] = [];
+  $: selectedTag = $page.url.searchParams.get("tag");
+  let selectedTagScorePercentile: number[];
 
   const getTagRandomDelay = (): string => {
     return (Math.random() * (0 - 1.2) + 1.2).toFixed(4);
   };
+
+  const fetchTagScoreRanking = async (tag: string): Promise<void> => {
+    try {
+      const res = await (
+        await fetch(
+          `${API_BASEURL}/badges-league/badges/${encodeURIComponent(tag)}`
+        )
+      ).json();
+      selectedTagScorePercentile = res.scorePercentile;
+    } catch (error) {}
+  };
+
+  $: {
+    if (selectedTag) {
+      fetchTagScoreRanking(selectedTag);
+    }
+  }
 
   onMount(async () => {
     tags = await (await fetch(`${API_BASEURL}/badges-league/badges`)).json();
@@ -19,20 +39,38 @@
 
 <div class="d-flex justify-content-center flex-wrap mt-5">
   {#each tags as tag}
-    <span
-      class="so-tag rounded-2 text-bg-dark m-1 p-1 px-2"
-      style="animation-delay: {getTagRandomDelay()}s;">{tag}</span
+    <a
+      href={"?tag=" + encodeURIComponent(tag)}
+      class="so-tag rounded-2 text-bg-dark m-1 p-1 px-2 text-decoration-none"
+      class:selected={tag === selectedTag}
+      style="animation-delay: {getTagRandomDelay()}s;">{tag}</a
     >
   {/each}
 </div>
 
-<style>
+{#if selectedTag}
+  {selectedTag}
+{/if}
+
+<style lang="scss">
   .so-tag {
     opacity: 0;
     animation: apparition 0.3s forwards;
     color: #9cc1db;
     background-color: #3d4951;
     font-size: 0.8rem;
+    border: solid 2px transparent;
+
+    &:hover,
+    &:focus,
+    &.selected {
+      color: #cde1ee;
+      background-color: #435460;
+    }
+
+    &.selected {
+      border-color: #cde1ee;
+    }
   }
 
   @keyframes apparition {

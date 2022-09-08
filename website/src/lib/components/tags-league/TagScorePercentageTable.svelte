405e87f0fd6dabbb4aebde8482942a83e02cb921
johannchopin <johannchopin@pm.me>
2022-09-08 20:12:32 +0200

    scrollable table

--- website/src/lib/components/tags-league/TagScorePercentageTable.svelte
+++ website/src/lib/components/tags-league/TagScorePercentageTable.svelte
@@ -2,19 +2,34 @@
   export let scorePercentages: [number, number][];
 </script>
 
-<table class="table table-dark table-sm table-bordered w-fit-content rounded-2">
-  <thead>
-    <tr>
-      <th>Top Percentage</th>
-      <th>Required Score</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each scorePercentages as scorePercentages}
+<div class="tableFixHead w-fit-content">
+  <table
+    class="table table-dark table-sm table-bordered w-fit-content rounded-2"
+  >
+    <thead>
       <tr>
-        <td>{scorePercentages[1]}%</td>
-        <td>{scorePercentages[0]}</td>
+        <th>Top Percentage</th>
+        <th>Required Score</th>
       </tr>
-    {/each}
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      {#each scorePercentages as scorePercentages}
+        <tr>
+          <td>{scorePercentages[1]}%</td>
+          <td>{scorePercentages[0]}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
+
+<style>
+  .tableFixHead {
+    overflow-y: auto;
+    height: 300px;
+  }
+  .tableFixHead thead th {
+    position: sticky;
+    top: 0;
+  }
+</style>

67f5393e58ebe5f12f5760806cb7d55fb4dd0e80
johannchopin <johannchopin@pm.me>
2022-09-17 15:03:21 +0200

    back to push for proper rendering

--- src/tags-league/helpers/getScrapedPopularTags.ts
+++ src/tags-league/helpers/getScrapedPopularTags.ts
@@ -25,7 +25,7 @@ const getScrapedPopularTags = async (): Promise<string[]> => {
 
   TAGS_TO_INCLUDE.forEach(tagToInclude => {
     if (!fetchedTags.includes(tagToInclude)) {
-      fetchedTags.unshift(tagToInclude)
+      fetchedTags.push(tagToInclude)
     }
   })
 

6ee7963e8ce6ef3a7968550397f985421e1dd0e0
johannchopin <johannchopin@pm.me>
2022-09-18 13:49:23 +0200

    serve tags-league ranking badge

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 
-import { SECONDS_IN_MIN, Template } from './const'
+import { SECONDS_IN_MIN, Template, THEMES } from './const'
 import { getProfileSvg } from './index'
 
-import { renderError, Theme } from './templates'
+import { renderError, renderRankingBadge, Theme } from './templates'
 import { isTemplateValid, isThemeValid } from './utils'
 import { initDatabase } from './db/init'
 import { getAnalytics } from './analytics'
 import scraperRouter from './tags-league/api/router'
+import { isTagInLeague } from './tags-league/utils'
+import { getUserRank } from './tags-league/helpers/getUserRank'
+import { getManager } from 'typeorm'
 
 const checkQueryStrings = (query: { theme: string; website?: string; location?: string }): void => {
   const { theme, website, location } = query
@@ -40,8 +43,32 @@ const run = async (): Promise<void> => {
     res.json(await getAnalytics())
   })
 
-  app.get('/:template/:id', async (req, res) => {
-    const { id, template } = req.params
+  app.get('/tags-league-ranking/:tagName/:userId', async (req, res) => {
+    const { tagName, userId } = req.params
+    const { theme = 'default' } = req.query
+    const manager = getManager()
+
+    try {
+      const isUserIdNumber = /^\d+$/.test(userId)
+      if (!isUserIdNumber) throw new Error('Given user id should be a number')
+
+      const isTagValid = await isTagInLeague(tagName)
+      if (!isTagValid) throw new Error(`The provided tag ${tagName} is not currently part of the Tags League.`)
+
+      const userRank = await getUserRank(manager, Number(userId), tagName)
+      if (userRank === undefined) throw new Error("This user doesn't have a sufficient score to be part of the Tags League or isn't a StackOverflow member!")
+
+      res.setHeader('Content-Type', 'image/svg+xml')
+      res.setHeader('Cache-control', `public, max-age=${CACHE_PERIOD}`)
+
+      res.send(renderRankingBadge(tagName, userRank.topPercentage, THEMES[theme as Theme] || THEMES.default))
+    } catch (error) {
+      res.send(renderError({ error: (error as Error).message }))
+    }
+  })
+
+  app.get('/:template/:userId', async (req, res) => {
+    const { userId, template } = req.params
     const { theme = 'default', website = 'true', location = 'true' } = req.query
 
     const templateValid = isTemplateValid(template)
@@ -55,7 +82,7 @@ const run = async (): Promise<void> => {
       checkQueryStrings({ theme, website, location })
 
       const svg = await getProfileSvg(
-        Number(id),
+        Number(userId),
         template as Template,
         {
           theme: theme as unknown as Theme,

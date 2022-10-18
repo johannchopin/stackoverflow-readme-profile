0edd90c7a7e71271083ee46c01513dbe3e6cc585
johannchopin <johannchopin@pm.me>
2022-10-18 09:51:04 +0200

    add badge preview

--- website/src/routes/tags-league/[tagName]/users/[userId]/+page.svelte
+++ website/src/routes/tags-league/[tagName]/users/[userId]/+page.svelte
@@ -4,17 +4,21 @@
 
   import { API_BASEURL } from "$lib/constants";
   import UsersRepartionByTag from "$lib/components/tags-league/graphs/UsersRepartionByTag.svelte";
-  import SoTagLink from "$lib/components/tags-league/SoTagLink.svelte";
   import LoadingAnimation from "$lib/components/tags-league/LoadingAnimation.svelte";
   import UnknownTagError from "$lib/components/tags-league/UnknownTagError.svelte";
   import UnknownUserError from "$lib/components/tags-league/UnknownUserError.svelte";
   import UserDetails from "$lib/components/tags-league/UserDetails.svelte";
   import LastUpdate from "$lib/components/LastUpdate.svelte";
   import Ranking from "$lib/components/icons/Ranking.svelte";
+  import BadgePreview from "./components/BadgePreview.svelte";
+  import ThemesSelect from "$lib/components/ThemesSelect.svelte";
+
+  const BADGE_PREVIEW_ID = "badgePreview";
 
   let tag = $page.params.tagName;
   let userId = $page.params.userId;
 
+  let showBadgePreview = false;
   let scorePercentages: [number, number][];
   let percentageAmounts: [number, number][];
   let score: number;
@@ -24,6 +28,7 @@
     invalidTag: false,
     invalidUser: false,
   };
+  let theme = "default";
 
   const fetchTagScoreRanking = async (tag: string): Promise<void> => {
     try {
@@ -92,10 +97,48 @@
 
   <UserDetails {userId} {score} {tag} {topPercentage} />
 
+  <div class="d-flex justify-content-center mt-4">
+    <a
+      href={"#" + BADGE_PREVIEW_ID}
+      class="btn btn-outline-primary btn-sm m-auto"
+      on:click={() => (showBadgePreview = true)}
+    >
+      Get your badge
+    </a>
+  </div>
+
   <div class="row">
     <hr
       class="border border-primary border-1 col-8 col-md-6 m-auto opacity-75 my-4"
     />
+  </div>
+
+  <div id={BADGE_PREVIEW_ID} class:open={showBadgePreview}>
+    {#if showBadgePreview}
+      <h2 class="mb-4">Badge preview</h2>
+
+      <div class="container">
+        <h3 class="fs-4">
+          <span class="fw-bold text-primary">1.</span>
+          Select the theme:
+        </h3>
+
+        <ThemesSelect bind:theme class="d-flex flex-wrap" />
+
+        <h3 class="fs-4 mt-5 mb-3">
+          <span class="fw-bold text-primary">2.</span>
+          Copy the Markdown/HTML into your README:
+        </h3>
+
+        <BadgePreview {tag} {userId} {theme} />
+      </div>
+    {/if}
+
+    <div class="row mt-5">
+      <hr
+        class="border border-primary border-1 col-8 col-md-6 m-auto opacity-75 my-4"
+      />
+    </div>
   </div>
 
   <div class="row justify-content-center mt-5">
@@ -121,10 +164,30 @@
     height: 32vw;
   }
 
+  #badgePreview {
+    overflow: hidden;
+    height: 0px;
+    transition: height 0.5s;
+
+    &.open {
+      height: 600px;
+      animation: open 1s forwards;
+      animation-delay: 0.5s;
+    }
+  }
+
   @include media-breakpoint-up(md) {
     :global(.rank) {
       width: 9vw;
       height: 9vw;
+    }
+  }
+
+  @keyframes open {
+    from {
+    }
+    to {
+      height: fit-content;
     }
   }
 </style>

531cc6baf81ba9e782c089fc8bdeb1413aa67d88
johannchopin <johannchopin@pm.me>
2022-10-22 18:43:49 +0200

    ⚰️ no need to compute top users

--- src/tags-league/ApiService.ts
+++ src/tags-league/ApiService.ts
@@ -42,69 +42,6 @@ export class ApiService {
     })
   }
 
-  // return type is [userId, userScore]
-  public getTopUsersByTag = async (tag: string, signal: AbortSignal): Promise<[number, number][] | undefined> => {
-    const headers = this.getHeaders()
-
-    const params = new URLSearchParams()
-    params.append('tagName', tag)
-
-    try {
-      Logger.log(`Start job for tag: ${tag}`)
-
-      const jobIdResponse = await fetch(
-        QUERY.LIST_USERS_BY_SCORE,
-        {
-          method: 'POST',
-          headers,
-          body: params
-        }
-      )
-
-      const response = await jobIdResponse.json()
-      const jobId = await response.job_id
-
-      if (jobId === undefined) {
-        // job didn't started. Possible reason is that the SEDE cookie is no more valid
-
-        storeLog(LogType.ERROR, response)
-        Logger.log(response)
-        throw new Error('Invalid SEDE cookie')
-      }
-
-      try {
-        Logger.log(`Start fetching top users for tag: ${tag}`)
-        const data = await this.getJobResponse(jobId, signal)
-
-        if (data) {
-          const topUsers: [number, number][] = []
-
-          // @ts-ignore
-          data.resultSets[0].rows.forEach((row) => {
-            const topUserId = row[0].id
-            const topUserScore = row[1]
-            const topUser: [number, number] = [topUserId, topUserScore]
-            topUsers.push(topUser)
-          })
-
-          Logger.log(`Fetched top users for tag: ${tag}`)
-
-          return topUsers
-        }
-        return []
-      } catch (error) {
-        Logger.log('Error trying to get response of a query job')
-        Logger.log(error)
-        return undefined
-      }
-    } catch (error) {
-      Logger.log('Error trying to start a query job')
-      Logger.log(error)
-
-      return undefined
-    }
-  }
-
   public getAmountUsersByScoreByTag = async (
     tag: string,
     signal: AbortSignal
--- src/tags-league/SedeQueries/constants.ts
+++ src/tags-league/SedeQueries/constants.ts
@@ -1,5 +1,4 @@
 export const QUERY = {
   CHECK_AUTH_COOKIE: 'https://data.stackexchange.com/query/run/1/1631255/1988830',
-  COUNT_USERS_SCORE_TAG: 'https://data.stackexchange.com/query/run/1/1631574/1989329',
-  LIST_USERS_BY_SCORE: 'https://data.stackexchange.com/query/run/1/1629390/1986822'
+  COUNT_USERS_SCORE_TAG: 'https://data.stackexchange.com/query/run/1/1631574/1989329'
 }
